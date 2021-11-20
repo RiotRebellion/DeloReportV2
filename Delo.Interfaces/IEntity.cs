e6eef0fc2068f7888ab89fce46008c16581bdd77
RiotRebellion <misha.dzhurmiy@gmail.com>
2021-11-20 22:27:10 +0800

    add repository interface

---
+++ Delo.Interfaces/IEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Delo.Interfaces
+{
+    public interface IEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Delo.Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Delo.Interfaces
+{
+    public interface IRepository<T> where T : class, IEntity, new()
+    {
+        IQueryable<T> Items { get; }
+
+        T Get(int id);
+
+        Task<T> GetAsync(int id, CancellationToken cancel = default);
+    }
+}
